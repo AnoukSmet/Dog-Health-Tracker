8216d3019059e80b696222a284ca3099346bab75
AnoukSmet <smet.anouk@gmail.com>
2020-09-18 12:46:21 +0000

    Connected Flask to MONGODB

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+os.environ["MONGO_DBNAME"] = "dog_health_tracker"
+os.environ["MONGO_URI"] = "mongodb+srv://root:r00tUser@myfirstcluster.djp7i.mongodb.net/dog_health_tracker?retryWrites=true&w=majority"
