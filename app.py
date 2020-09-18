aff87ec1c64e575b798226862a8280610cc5e305
AnoukSmet <smet.anouk@gmail.com>
2020-09-18 14:21:01 +0000

    Displayed profile on screen in dashboard.html

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
+
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
@@ -17,12 +18,10 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/view_profile')
 def view_profile():
-    return render_template("dashboard.html", dogs=mongo.db.dogs())
+    return render_template("dashboard.html", dogs=mongo.db.dogs.find())
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-
-
